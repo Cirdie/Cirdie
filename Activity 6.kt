bd5c4b70cd614abdc216eb9e521602fa510e6e21
Luna <159744316+Cirdie@users.noreply.github.com>
2024-03-05 19:54:52 +0800

    Update Activity 6.kt

--- Activity 6.kt
+++ Activity 6.kt
@@ -1,47 +1,47 @@
 fun main() {
+    // Prompting user to input two strings
     println("Enter First Character:")
-    val character1 = readLine()?.toLowerCase() ?: ""
+    var character1 = readLine() ?: ""
 
     println("Enter Second Character:")
-    val character2 = readLine()?.toLowerCase() ?: ""
+    var character2 = readLine() ?: ""
 
+    // Finding unique characters
     var uniqueChars = ""
 
+    // Loop through character1 to find unique characters
     for (c1 in character1) {
-        var found = false // Initialize a variable to track if character is found in 'character2'
-
+        var found = 0
         for (c2 in character2) {
-            if (c1 == c2) {
-                found = true // Set 'found' to true if character is found
-                break // Exit the loop as the character is already found
+            if (c1.equals(c2, ignoreCase = true)) {
+                found = 1
+                break
             }
         }
-
-        if (!found && !uniqueChars.contains(c1)) {
+        if (found == 0 && !uniqueChars.contains(c1, ignoreCase = true) &&
+            character1.count { it.equals(c1, ignoreCase = true) } == 1 &&
+            character2.count { it.equals(c1, ignoreCase = true) } == 0
+        ) {
             uniqueChars += c1
         }
     }
 
+    // Loop through character2 to find unique characters
     for (c2 in character2) {
-        var found = false // Initialize a variable to track if character is found in 'character1'
-
-        for (c1 in character1) {
-            if (c2 == c1) {
-                found = true // Set 'found' to true if character is found
-                break // Exit the loop as the character is already found
-            }
-        }
-
-        if (!found && !uniqueChars.contains(c2)) {
+        if (!character1.contains(c2, ignoreCase = true) &&
+            !uniqueChars.contains(c2, ignoreCase = true) &&
+            character2.count { it.equals(c2, ignoreCase = true) } == 1
+        ) {
             uniqueChars += c2
         }
     }
 
-    if (uniqueChars.isEmpty() || character1 == character2) {
+    // Displaying the result
+    if (uniqueChars.isEmpty()) {
         println("No Unique Character")
     } else {
         println("Unique Characters are")
-        var index = 1 // Initialize index counter for printing
+        var index = 1
         for (c in uniqueChars) {
             println("[$index] $c")
             index++
